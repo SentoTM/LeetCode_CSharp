737a3c846b152bfcb861486e20961317c16864f8
Vicente Tormo <vicent.tormo@gmail.com>
2024-01-25 01:06:55 +0100

    TwoSum solucion y pruebas en main.

---
+++ LC_SolucionesCSharp/LC_TwoSum/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace LC_TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            //Ejemplo 1. la solucion debería ser los índices 0 y 1.
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            
+            try
+            {
+                int[] result = solution.TwoSum(nums, target);
+                Console.WriteLine($"Indices que suman al objetivo: [{result[0]}, {result[1]}]");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            };
+
+            //Ejemplo 2. la solucion debería ser los índices 1 y 2.
+            int[] nums2 = { 3, 2, 4 };
+            target = 6;
+
+            try
+            {
+                int[] result = solution.TwoSum(nums2, target);
+                Console.WriteLine($"Indices que suman al objetivo: [{result[0]}, {result[1]}]");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            //Ejemplo 3. la solucion debería ser los índices 0 y 1.
+            int[] nums3 = { 3, 3 };
+            target = 6;
+
+            try
+            {
+                int[] result = solution.TwoSum(nums3, target);
+                Console.WriteLine($"Indices que suman al objetivo: [{result[0]}, {result[1]}]");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            //Ejemplo 4. la solucion debería ser error.
+            int[] nums4 = { 3, 3 };
+            target = 7;
+
+            try
+            {
+                int[] result = solution.TwoSum(nums3, target);
+                Console.WriteLine($"Indices que suman al objetivo: [{result[0]}, {result[1]}]");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
+
+
+
+
---
+++ LC_SolucionesCSharp/LC_TwoSum/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LC_TwoSum
+{
+    // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    // You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    // You can return the answer in any order.
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            throw new ArgumentException("No se ha encontrado solucion");
+        }
+    }
+}
